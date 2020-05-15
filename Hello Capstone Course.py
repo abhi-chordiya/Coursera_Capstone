3af87ae4fbc2afa25cc217e8001f46efaac3c878
abhi-chordiya <62467427+abhi-chordiya@users.noreply.github.com>
2020-05-15 10:14:21 +0530

    Add files via upload

---
+++ Hello Capstone Course.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This Notebook is used for coursera Capstone Project course. 
+# this note book contains first week assignment
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print(" Hello Capstone Project Course! ")
+
+
+# In[ ]:
+
+
+
+
